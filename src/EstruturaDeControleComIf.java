6b60723a0278f32e7c16fd990b7b71e74e31789e
natan <natanaelmuller730@gmail.com>
2023-08-14 20:21:54 -0300

    Primeira Aula

--- src/EstruturaDeControleComIf.java
+++ src/EstruturaDeControleComIf.java
@@ -1,5 +1,29 @@
 public class EstruturaDeControleComIf {
 
+    public static void main(String[] args) {
+        String nome = "Lucas";
+        double peso = 100.0;
+        double altura = 1.70;
+        double imc = peso / (altura * altura);
 
+        System.out.printf("\nO IMC do %s é: %.2f",nome, imc);
+
+        boolean resultado = imc < 18.5;
+
+        System.out.println(resultado);
+
+        if (resultado){
+            System.out.println("Abaixo do peso ideal");
+        }else if (imc >= 18.5 && imc < 25){
+            System.out.println("Peso ideal");
+        }
+        else if (imc>= 25 && imc<30){
+            System.out.println("Obesidade de grau 1");
+        } else if (imc >= 30 && imc<40) {
+            System.out.println("Obesidade de grau 2");
+        } else{
+            System.out.println("Obesidade grau 3");
+        }
+    }
 
 }
